f89898e117ae199168af397b4266e24f34272c25
Zhihao Ni <xdnizhihao@gmail.com>
2024-11-12 00:51:06 -0800

    add feedback form

---
+++ components/makeform/emb-feedback-button.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { LifeBuoy } from 'lucide-react'
+import Script from 'next/script'
+
+const CONTAINER_ID = 'feedback-button-container'
+const FORM_ID = 'XLqG6eTH'
+const popUpPosition = 'top-left'
+
+export const EmbeddedFeedbackButton = () => (
+  <>
+    <iframe
+      src={`https://makeform.ai/e/${FORM_ID}`}
+      style={{
+        position: 'absolute',
+        left: '-9999px',
+        width: '340px',
+        visibility: 'hidden',
+      }}
+    />
+    <Script
+      src={`https://makeform.ai/feedback-bundle.js`}
+      strategy="lazyOnload"
+      // @ts-expect-error script attributes are injected by external script
+      containerId={CONTAINER_ID}
+      position={popUpPosition}
+      formId={FORM_ID}
+      buttonClassName={"pl-4 pr-2 pt-2 pb-1 rounded-tl-2xl bg-white border border-gray-200 text-sm hover:text-purple-700 hover:scale-110 transition-all duration-300"}
+    />
+    <div id={CONTAINER_ID} />
+  </>
+)
+
